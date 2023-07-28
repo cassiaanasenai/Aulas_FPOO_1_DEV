1858c750e3b34423dba9774d63d6f0e2b0e1bd99
cassiaanasenai <ana.silva17@aluno.senai.br>
2023-07-28 11:06:26 -0300

    Entendendo Wrappers

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -20,7 +20,9 @@ public class Fundamentos {
         double media = 5.5;
         
         String bairro = new String("Centro");
-        
+        int nota1 = Integer.parseInt("8");
+        double mediaF = Double.parseDouble("5.5");
+        boolean aprovado = Boolean.parseBoolean("true");
     }
     
 }
