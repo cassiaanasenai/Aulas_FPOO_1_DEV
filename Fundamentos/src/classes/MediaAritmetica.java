1dde062990500b10902831812fbc6ef461a5316a
cassiaanasenai <ana.silva17@aluno.senai.br>
2023-08-03 13:21:02 -0300

    envio da atividade

--- Fundamentos/src/classes/MediaAritmetica.java
+++ Fundamentos/src/classes/MediaAritmetica.java
@@ -3,11 +3,6 @@ package classes;
 import java.util.Scanner;
 
 public class MediaAritmetica {
-     public static void main(String[] args) {
-       for (int i = 1; i <=100; i++)
-           if (i % 2 == 0){
-               System.out.println(i);
-       
-           }          
-         }
     }
+
+   
--- Fundamentos/src/classes/NumerosPares.java
+++ Fundamentos/src/classes/NumerosPares.java
@@ -1,5 +1,10 @@
 package classes;
 
 public class NumerosPares {
-
+     public static void main(String[] args) {
+       for (int i = 1; i <=100; i++)
+           if (i % 2 == 0){
+               System.out.println(i);
+           }          
+         }
 }
