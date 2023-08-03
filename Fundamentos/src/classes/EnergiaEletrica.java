0c42f9171e18dcbda416884074accc287c0048e1
cassiaanasenai <ana.silva17@aluno.senai.br>
2023-08-03 14:43:13 -0300

    envio da atividade

--- Fundamentos/src/classes/EnergiaEletrica.java
+++ Fundamentos/src/classes/EnergiaEletrica.java
@@ -1,5 +1,43 @@
 package classes;
 
-public class EnergiaEletrica {
+import com.sun.source.tree.IfTree;
+import java.util.Scanner;
 
-}
+public class EnergiaEletrica {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Quantidade de kw/h consumido: ");
+        double qtdeKwh = scanner.nextDouble();
+        
+        System.out.println("Tipo de instalacao eletrica: (R para residencial, I para industria, C para comercio):");
+        char tipoInstalacao = scanner.next().charAt(0);
+        
+        double precoTotal = 0;
+        if (tipoInstalacao == 'R') {
+          if (qtdeKwh <= 500) {
+              precoTotal = qtdeKwh * 0.40; 
+        }else{
+              precoTotal = qtdeKwh * 0.65;
+          }else if (tipoInstalacao == 'C') {
+               if (qtdeKwh <= 1000) {
+                  precoTotal = qtdeKwh * 0.55;   
+             }else{
+                  precoTotal = qtdeKwh *0.60;
+                  }
+            }else{
+            System.out.println("Tipo de instalacao invalida");
+            return;         
+    } 
+          System.out.println("O valor a pagar e = R$" + precoTotal);
+          
+     }else if (tipoInstalacao == 'I'){
+               if (qtdeKwh <= 5000) {
+                  precoTotal = qtdeKwh * 0.55;   
+             }else{
+                  precoTotal = qtdeKwh *0.60;
+    }
+    
+        }
+   
+            }
--- Fundamentos/src/classes/MediaAritmetica.java
+++ Fundamentos/src/classes/MediaAritmetica.java
@@ -3,6 +3,22 @@ package classes;
 import java.util.Scanner;
 
 public class MediaAritmetica {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Quantidade de valores: ");
+        int qtdeValores = scanner.nextInt();
+        
+        double soma = 0;
+        
+        for (int i = 0; i < qtdeValores; i++){
+            System.out.println("Coloque o valor: " + (i + 1)+ ":");
+            double valor = scanner.nextDouble();
+            soma += valor;
+        }
+        double media = soma / qtdeValores;
+        System.out.println("A media aritmetica e = " + media);
+    }
     }
 
    
